38c3ead42547536fbbcb7fb5c076b7b3fb68bda1
Einstein1337 <pacifico4@gmx.ch>
2021-07-07 11:51:52 +0200

    Working NN

--- neural_network/04_neural_network.py
+++ neural_network/04_neural_network.py
@@ -4,21 +4,37 @@ from numpy.core.numeric import outer
 import os
 
 
-def saveBestWeightsLR(precision_tp):
-    with open('Best_weights_learning_rate_tp.txt', 'r') as f:
-        file_lines = f.readlines()
-    highest_percentage = float(file_lines[0])
-    if precision_tp > highest_percentage:
-        f = open('Best_weights_learning_rate.txt', 'w')
-        f.write(f"{precision_toy_problem}\n")
-        f.close()
-        f = open('Best_weights_learning_rate.txt', 'a')
-        f.write(f"{toy_problem_network.learning_rate}\n")
-        f.write(f"{toy_problem_network.W}")
+def saveBestWeightsLR(precision, network, nn_name):
+    if nn_name == "t":
+        with open('Best_weights_learning_rate_tp.txt', 'r') as f:
+            file_lines = f.readlines()
+        highest_percentage = float(file_lines[0])
+        if precision > highest_percentage:
+            f = open('Best_weights_learning_rate_tp.txt', 'w')
+            f.write(f"{precision}\n")
+            f.close()
+            f = open('Best_weights_learning_rate_tp.txt', 'a')
+            f.write(f"{network.learning_rate}\n")
+            f.write(f"{network.W}")
+
+    elif nn_name == "m":
+        with open('Best_weights_learning_rate_mnist.txt', 'r') as f:
+            file_lines = f.readlines()
+        highest_percentage = float(file_lines[0])
+        if precision > highest_percentage:
+            f = open('Best_weights_learning_rate_mnist.txt', 'w')
+            f.write(f"{precision}\n")
+            f.close()
+            f = open('Best_weights_learning_rate_mnist.txt', 'a')
+            f.write(f"{network.learning_rate}\n")
+            f.write(f"{network.W}")
+
+        
 
 
 class Network:
-    def __init__(self, neuron_list, learning_rate):
+    def __init__(self, neuron_list, learning_rate, name):
+        self.name = name
         self.neuron_list = neuron_list
         self.input_neurons = neuron_list[0]
         self.output_neurons = neuron_list[-1]
@@ -27,16 +43,10 @@ class Network:
         self.W = []
         for layer in range(len(neuron_list)-1):
             self.W.append(np.random.uniform(-0.5, 0.5, (neuron_list[layer+1], neuron_list[layer])))
-
-        # self.W[0] = np.array([[0.36, 0.13, 0.3, -0.37], 
-        #                       [-0.46, 0.25, 0.27, 0.2], 
-        #                       [0.25, -0.19, -0.2, -0.17]])
-
-        # self.W[1] = np.array([[0.19, -0.43, 0.28], 
-        #                       [-0.21, 0.03, -0.26]])
-
+        
     def sigmoid(self, z):
         return 1/(1+np.exp(-z))
+
 
     def feedforward(self, input_array):
         next_hidden_layer_array = input_array
@@ -56,15 +66,16 @@ class Network:
         for e in range(len(E_out)):
             Cost += np.power(E_out[e], 2)
         return 0.5*Cost
+
     def train(self, input_list):
         for line in range(len(input_list)):
             if line%200 == 0:
-                print(f"Training: {int(line/len(input_list) * 100)}%", end="\r")
+                print(f"Training {self.name}: {int(line/len(input_list) * 100)}%", end="\r")
             split_input_list = input_list[line].split(",")
             split_input_list = [float(i) for i in split_input_list]
-            target_index = int(split_input_list[0])#1
+            target_index = int(split_input_list[0])
             target_array = self.getTargetArray(target_index)
-            input_array = np.array(split_input_list[1:len(split_input_list)])/255#np.array([1, 0.98, 0.76, 0.58])
+            input_array = np.array(split_input_list[1:len(split_input_list)])/255
 
             output_array = self.feedforward(input_array)
             self.hidden_layer_arrays.reverse()
@@ -80,13 +91,13 @@ class Network:
             W_new.reverse()
             self.W = W_new
             self.hidden_layer_arrays = []
+        print(f"Training {self.name}: 100%")
         
-            
-
-
     def test(self, input_list):
         right_answers = 0
         for line in range(len(input_list)):
+            if line%100 == 0:
+                print(f"Test {self.name}: {int(line/len(input_list) * 100)}%", end="\r")
             split_input_list = input_list[line].split(",")
             split_input_list = [float(i) for i in split_input_list]
             target = int(split_input_list[0])
@@ -99,11 +110,13 @@ class Network:
             if highest_value_index == target:
                 right_answers += 1
 
+        print(f"Test {self.name}: 100%")
         return right_answers/len(input_list)*100
 
 
 
-toy_problem_network = Network([4, 3, 2], 0.0121)
+toy_problem_network = Network([4, 3, 2], 0.125, "toy problem network")
+mnist_network = Network([784, 50, 50, 10], 0.01, "MNIST network")
 
 # get inputs and target from csv file
 with open('data\data_dark_bright_training_20000.csv', 'r') as f1tr:
@@ -112,12 +125,22 @@ with open('data\data_dark_bright_training_20000.csv', 'r') as f1tr:
 with open('data\data_dark_bright_test_4000.csv', 'r') as f1ts:
     input_list_toy_problem_test = f1ts.readlines()
 
-toy_problem_network.__init__([4, 3, 2], 0.0121)
+with open('data\mnist_train.csv', 'r') as f2tr:
+    input_list_mnist_train = f2tr.readlines()
+
+with open('data\mnist_test.csv', 'r') as f2ts:
+    input_list_mnist_test = f2ts.readlines()
+
+        
 toy_problem_network.train(input_list_toy_problem_train)
+mnist_network.train(input_list_mnist_train)
 
 precision_toy_problem = toy_problem_network.test(input_list_toy_problem_test)
+precision_mnist = mnist_network.test(input_list_mnist_test)
 
-saveBestWeightsLR(precision_toy_problem)
 
-print("Training: 100%")
+saveBestWeightsLR(precision_toy_problem, toy_problem_network, "t")
+saveBestWeightsLR(precision_mnist, "m")
+
 print(f"Toy problem network precision: {precision_toy_problem}%")
+print(f"MNIST network precision: {precision_mnist}%")
