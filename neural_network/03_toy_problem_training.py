10426b5d66ede8725b8ab5f7f37e4428b3b785e6
Einstein1337 <pacifico4@gmx.ch>
2021-07-07 09:37:01 +0200

    working training function, visualized progress of training

--- neural_network/03_toy_problem_training.py
+++ neural_network/03_toy_problem_training.py
@@ -1,12 +1,21 @@
 import numpy as np
+from numpy.core.fromnumeric import transpose
 from numpy.core.numeric import outer
 import os
 
-def clearConsole():
-    command = 'clear'
-    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
-        command = 'cls'
-    os.system(command)
+
+def saveBestWeightsLR(precision_tp):
+    with open('Best_weights_learning_rate.txt', 'r') as f:
+        file_lines = f.readlines()
+    highest_percentage = float(file_lines[0])
+    if precision_tp > highest_percentage:
+        f = open('Best_weights_learning_rate.txt', 'w')
+        f.write(f"{precision_toy_problem}\n")
+        f.close()
+        f = open('Best_weights_learning_rate.txt', 'a')
+        f.write(f"{toy_problem_network.learning_rate}\n")
+        f.write(f"{toy_problem_network.W}")
+
 
 class Network:
     def __init__(self, neuron_list, learning_rate):
@@ -42,13 +51,20 @@ class Network:
         target_array[index] = 1
         return target_array
 
+    def Cost(self, E_out):
+        Cost = 0
+        for e in range(len(E_out)):
+            Cost += np.power(E_out[e], 2)
+        return 0.5*Cost
     def train(self, input_list):
-        for line in range(1):
+        for line in range(len(input_list)):
+            if line%200 == 0:
+                print(f"Training: {int(line/len(input_list) * 100)}%", end="\r")
             split_input_list = input_list[line].split(",")
             split_input_list = [float(i) for i in split_input_list]
-            target_index = 1#int(split_input_list[0])
+            target_index = int(split_input_list[0])#1
             target_array = self.getTargetArray(target_index)
-            input_array = np.array([1, 0.98, 0.76, 0.58])#np.array(split_input_list[1:len(split_input_list)])/255
+            input_array = np.array(split_input_list[1:len(split_input_list)])/255#np.array([1, 0.98, 0.76, 0.58])
 
             output_array = self.feedforward(input_array)
             self.hidden_layer_arrays.reverse()
@@ -57,17 +73,14 @@ class Network:
             W_new = []
             E_hidden = E_out
             for weights in range(len(self.W)):
-                transposed_array = np.reshape(self.hidden_layer_arrays[weights+1], (1, -1))
-                W_updated = self.W[weights] + self.learning_rate*np.dot(np.reshape((E_hidden*self.hidden_layer_arrays[weights]*(1-self.hidden_layer_arrays[weights])), (-1, 1)), transposed_array)
+                W_updated = self.W[weights] + self.learning_rate*np.dot(np.reshape((E_hidden*self.hidden_layer_arrays[weights]*(1-self.hidden_layer_arrays[weights])), (-1, 1)), np.reshape(self.hidden_layer_arrays[weights+1], (-1, 1)).transpose())
                 W_new.append(W_updated)
-                print(self.W)
-                print(self.W.T)
-                E_hidden = np.dot(self.W[weights].T, E_hidden)
-                
-
+                E_hidden = np.dot(self.W[weights].transpose(), E_hidden)       
+            
             W_new.reverse()
             self.W = W_new
             self.hidden_layer_arrays = []
+        
             
 
 
@@ -91,7 +104,7 @@ class Network:
 
 
 
-toy_problem_network = Network([4, 3, 2], 0.01)
+toy_problem_network = Network([4, 3, 2], 0.0115)
 #MNIST_network = Network([784, 100, 100, 10], 1)
 
 # get inputs and target from csv file
@@ -101,13 +114,17 @@ with open('data\data_dark_bright_training_20000.csv', 'r') as f1tr:
 with open('data\data_dark_bright_test_4000.csv', 'r') as f1ts:
     input_list_toy_problem_test = f1ts.readlines()
 
-for k in range(10):
-    toy_problem_network.train(input_list_toy_problem_train)
+
+toy_problem_network.train(input_list_toy_problem_train)
 # with open('data\mnist_test.csv', 'r') as f2:
 #     input_list_MNIST = f2.readlines()
 
 precision_toy_problem = toy_problem_network.test(input_list_toy_problem_test)
+
+saveBestWeightsLR(precision_toy_problem)
+
 #precision_MNIST = MNIST_network.test(input_list_MNIST)
 
+print("Training: 100%")
 print(f"Toy problem network precision: {precision_toy_problem}%")
 #print(f"MNIST network precision:       {precision_MNIST}%")
---
+++ neural_network/Vector_transpose_test.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+v = np.array([1, 2, 3])
+print(v)
+print(np.reshape(v, (-1, 1)).transpose())
